f310d7ef63541e9662e2cf7d1e68f82d7763030a
nakajmg <nakajima.jmg@gmail.com>
2018-03-12 00:07:42 +0900

    fix

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+function bindClassName(el, binding, vnode) {
+  const componentName = vnode.context.$options._componentTag
+  const className = `${componentName}${binding.value || ''}`
+  if (el.className.indexOf(className) !== -1) return
+  el.className = `${className}${el.className ? ' ' + el.className :''}`
+}
+
 const ClassName = {
   install(Vue) {
-    Vue.directive('className', (el, binding, vnode) => {
-      const componentName = vnode.context.$options._componentTag
-      const className = `${componentName}${binding.value || ''}`
-      if (el.className.indexOf(className) !== -1) return 
-      el.className = `${className}${el.className ? ' ' + el.className :''}`
+    Vue.directive('className', {
+      bind: bindClassName,
+      update: bindClassName,
+      inserted: bindClassName,
     })
   }
 }
