dab6ac8e0eedf69f0d9787aaf55f332a25c9e46f
nakajmg <nakajima.jmg@gmail.com>
2018-03-11 21:53:53 +0900

    fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const ClassName = {
     Vue.directive('className', (el, binding, vnode) => {
       const componentName = vnode.context.$options._componentTag
       const className = `${componentName}${binding.value || ''}`
+      if (el.className.indexOf(className) !== -1) return 
       el.className = `${className}${el.className ? ' ' + el.className :''}`
-
     })
   }
 }
